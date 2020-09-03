0c29505ef05dc3919c981b7b121878fbc3166bc0
Sergey Osipov <serge@osipoffphoto.com>
2020-09-03 22:08:11 +1000

    seeded cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'faker'
 puts "Cleaning DB"
 Ingredient.destroy_all
+Cocktail.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -27,9 +28,8 @@ puts "Created #{Ingredient.count} ingredients"
 
 # 30.times do
 # Cocktail.create!(
-#     name: Faker::Games::Pokemon.move
+# name: Faker::Games::Pokemon.move
 # )
-
 # end
 
 # puts "Created #{Cocktail.count} cocktails"
