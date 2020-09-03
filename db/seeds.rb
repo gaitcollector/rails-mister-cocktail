00704c2c5090398b14310ba0166e1ab93b7e014c
Sergey Osipov <serge@osipoffphoto.com>
2020-09-03 21:50:08 +1000

    in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require 'json'
+require 'open-uri'
+require 'faker'
+puts "Cleaning DB"
+Ingredient.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +10,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating ingredients..."
+result = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+drinks.each do |hash|
+  hash.each do |key, ingredient|
+    Ingredient.create(name: ingredient)
+  end
+end
+
+puts "Created #{Ingredient.count} ingredients"
+
+# sleep 01
+
+# puts "Creating cocktails..."
+
+# 30.times do
+# Cocktail.create!(
+#     name: Faker::Games::Pokemon.move
+# )
+
+# end
+
+# puts "Created #{Cocktail.count} cocktails"
